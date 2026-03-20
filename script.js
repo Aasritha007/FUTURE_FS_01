5c6127838b121aa425144986f70e5295e258d341
Aasritha007 <324103212042.aasritha@gvpcew.ac.in>
2026-03-20 16:59:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("Aasritha Portfolio Loaded 🚀");
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener("click", function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute("href")).scrollIntoView({
+            behavior: "smooth"
+        });
+    });
+});
