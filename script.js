a92c8b9054f8be3d747b321919c419b533f928cd
Aasritha007 <324103212042.aasritha@gvpcew.ac.in>
2026-03-20 17:12:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("Professional Portfolio Loaded 🚀");
+function openTab(tabName) {
+    let tabs = document.getElementsByClassName("tab-content");
+
+    for (let i = 0; i < tabs.length; i++) {
+        tabs[i].style.display = "none";
+    }
+
+    document.getElementById(tabName).style.display = "block";
+}
+
+// Default open
+document.getElementById("about").style.display = "block";
